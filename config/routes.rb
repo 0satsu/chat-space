f6aaf082f419152dd825bf3bab8c0b4020f2dc19
Osatsu-cat <moluca5460@gmail.com>
2019-06-16 15:12:51 +0900

    add creation-group

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users, only: [:new, :create, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :edit, :update, :index]
+  resources :groups, only:[:new, :create, :edit, :update] do
+    resources :messeges, only:[:index, :create]
+  end
 end
