0a03a7104f689a4fc8f79be3342684f31ae1033b
Osatsu-cat <moluca5460@gmail.com>
2019-06-22 16:56:44 +0900

    add auto-reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    var html = `<div class="chat__bottom__message">
+    var html = `<div class="chat__bottom__message" data-id =${message.id}  data-group_id = ${message.group_id} >
                   <div class="chat__bottom__message_info">
                     <div class="chat__bottom__message__info__user">
                       <p>${message.user}</p>
@@ -43,4 +43,34 @@ $(function(){
       alert("エラーが発生しました")
     })
   });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $(".chat__bottom__message").last().data("id");
+    var last_message_group = $(".chat__bottom__message").last().data("group-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "/groups/:group_id/api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id, group_id: last_message_group}
+    })
+    .done(function(messages) {
+       //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.forEach(function(message){
+        if(message.id > last_message_id && message.group_id == last_message_group){
+          insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+        }
+        $('.messages').append(insertHTML);
+        $('.chat__bottom').animate({ scrollTop: $('.chat__bottom')[0].scrollHeight},'fast');
+      });
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
-json.content @message.content
-json.image @message.image
+json.(@message, :content, :image)
 json.user @message.user.name
 json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.id @message.id
+json.group_id @message.group.id
