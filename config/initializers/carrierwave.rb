e2225af7ec4949d0a0d6a00ed1bad8bb85a26646
Osatsu-cat <moluca5460@gmail.com>
2019-06-26 14:12:54 +0900

    for image upload

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id, #大事！secrets.ymlに設定してそこから参照
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1' #アジアパシフィック（東京）の場合
+  }
+
+  config.fog_directory  = 'upload-for-chat-space'
+  config.asset_host = 'https://s3-\ap-northeast-1.amazonaws.com/upload-for-chat-space'
+end
