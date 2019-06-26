f46c1e887d073dcce175bfcf28bd8f87191e1712
Osatsu-cat <moluca5460@gmail.com>
2019-06-26 14:59:50 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'upload-for-chat-space'
-  config.asset_host = 'https://s3-\ap-northeast-1.amazonaws.com/upload-for-chat-space'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-for-chat-space'
 end
